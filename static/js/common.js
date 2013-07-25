766f6c81f6653d0a748152cab3bf2ef9c9f2e824
Zhang Shibing <shibingzhang@sohu-inc.com>
2013-07-25 17:40:10 +0800

    add qrcode

---
+++ static/js/common.js
@@ -0,0 +1,63 @@
+var DOC = document, WIN = window;
+
+$(function() {
+  //pjax
+  // var timer, elLoad = $('#loading');
+
+  // $(DOC).pjax('.yui3-pjax', '#left_con');
+
+  // $(DOC).on('pjax:send', function() {
+  //   timer = setTimeout(function() {
+  //     elLoad.show();
+  //     timer = null;
+  //   }, 1500);
+  // });
+
+  // $(DOC).on('pjax:complete', function() {
+  //   if (!timer) {
+  //     elLoad.hide();
+  //   } else {
+  //     clearTimeout(timer);
+  //   }
+  // });
+
+  //qrcode
+  $('.qrcode .btn').on('click', function() {
+    var $this = $(this), isActive = $this.hasClass('active');
+    $this.parent().animate({'top': !isActive ? '4px' : '-180px'}, {
+      duration: isActive ? 200 : 800,
+      easing: isActive ? 'easeInQuad' : 'easeOutBounce',
+      complete: function() {
+        isActive ? $this.removeClass('active') : $this.addClass('active');
+      }
+    });
+  });
+
+  //储存二维码状态
+    
+
+});
+
+
+/* YUI
+YUI().use('node', 'pjax', 'anim', function (Y) {
+
+  var pjax = new Y.Pjax({ container: '#left_con' }), body = Y.one('body'), t;
+
+  pjax.on('navigate', function (e) {
+    t = setTimeout(function() {
+    	Y.one('#loading').setStyle('display', 'block');
+    	t = null;
+    }, 1500);
+  });
+
+  pjax.on(['error', 'load'], function (e) {
+  	if (!t) {
+  		Y.one('#loading').setStyle('display', 'none');
+  	} else {
+  		clearTimeout(t);
+  	}
+  });
+
+});
+*/
